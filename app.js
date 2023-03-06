01a880a324649e75cfb49596d4200cefb37c2c8a
hastman1687 <hastamanjan@yahoo.com>
2023-03-06 16:30:28 +0500

    rock paper scissor game

---
+++ app.js
@@ -0,0 +1,25 @@
+const choices = ["rock", "paper", "scissors"];
+
+function playGame() {
+  const userChoice = prompt("Choose rock, paper, or scissors:");
+  const computerChoice = choices[Math.floor(Math.random() * 3)];
+
+  document.write(`You chose ${userChoice} and the computer chose ${computerChoice}.`);
+
+  if (userChoice === computerChoice) {
+    document.write("It's a tie!");
+  } else if (userChoice === "rock" && computerChoice === "scissors" ||
+             userChoice === "paper" && computerChoice === "rock" ||
+             userChoice === "scissors" && computerChoice === "paper") {
+    document.write("You win!");
+  } else {
+   document.write("The computer wins!");
+  }
+
+  const playAgain = prompt("Play again? (yes/no)").toLowerCase();
+  if (playAgain === "yes") {
+    playGame();
+  }
+}
+
+playGame();
